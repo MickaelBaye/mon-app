68547fd6f6bcd5c76165c79b97f301a79bafdcb3
Mickaël B <mickael.baye@gmail.com>
2022-01-03 22:44:06 +0100

    feat: init sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Sidebar from './Sidebar/Sidebar';
 
 function App() {
   return (
     <div className="App">
+      <Sidebar />
     </div>
   );
 }
---
+++ src/Sidebar/Sidebar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Sidebar.css";
+
+class Sidebar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return ( 
+      <div className="Sidebar">
+        <h1>Sidebar</h1>
+      </div>
+     );
+  }
+}
+ 
+export default Sidebar;
